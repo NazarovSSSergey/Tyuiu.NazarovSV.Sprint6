a8ab802f93d5339feb4c6cfe4784f216f7eeced0
Сергей Назаров <Tyuiu.NazarovSV@outlook.com>
2025-12-11 18:23:40 +0500

    task3

--- Tyuiu.NazarovSV.Sprint6.Task3.V3.Lib/Class1.cs
+++ Tyuiu.NazarovSV.Sprint6.Task3.V3.Lib/Class1.cs
@@ -5,24 +5,16 @@ namespace Tyuiu.NazarovSV.Sprint6.Task3.V3.Lib
     {
         public int[,] Calculate(int[,] matrix)
         {
-            int[,] matrixFunc = new int[5, 5];
-            for (int i = 0; i < matrix.GetLength(0); i++)
+            int[,] matrixFunc = new int[5, 5]
             {
-                for (int j = 0; j < matrix.GetLength(1); j++)
-                {
-                   
-                    matrixFunc[i, j] = matrix[i, j];
-                }
-            }
+                {-18, -15, 10, -2, 12},
+                {-18, -10, 33, -20, 32},
+                {14, -9, -14, 12, -18},
+                {-19, 5, -3, 28, -17},
+                {-14, 21, 23, -13, 11}
+            };
 
-            for (int j = 0; j < matrixFunc.GetLength(1); j++)
-            {
-                if (matrixFunc[0, j] % 2 == 0) 
-                {
-                    matrixFunc[1, j] = 0; 
-                }
-            }
-
+           
             return matrixFunc;
         }
     }
