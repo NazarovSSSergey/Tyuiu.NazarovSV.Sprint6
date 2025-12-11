8860e5e11db234444e2a1b47fe52fae150ad9dbd
Сергей Назаров <Tyuiu.NazarovSV@outlook.com>
2025-12-11 15:55:24 +0500

    Task1

--- Tyuiu.NazarovSV.Sprint6.Task1.V24.Lib/Class1.cs
+++ Tyuiu.NazarovSV.Sprint6.Task1.V24.Lib/Class1.cs
@@ -5,26 +5,24 @@ namespace Tyuiu.NazarovSV.Sprint6.Task1.V24.Lib
     {
         public double[] GetMassFunction(int startValue, int stopValue)
         {
-            double[] valueArray;
-            int len = (stopValue - startValue) + 1;
-            valueArray = new double[len];
-            double y;
-            int c = 0;
-            for (int x = startValue; x <= stopValue; x++)
+            int len = stopValue - startValue + 1;
+            double[] ArrayFunc = new double[len];
+
+            int Index = 0;
+            for (int i = startValue; i <= stopValue; i++)
             {
-                y = ( Math.Sin(x) / x + 1.7 ) - Math.Cos(x) * 4 * x - 6;
-                y = Math.Round(y, 2);
-                if (x + 1.7 == 0)
+                if ((i + 1.7) == 0)
                 {
-                    valueArray[c] = 0;
+                    ArrayFunc[Index] = 0;
                 }
                 else
                 {
-                    valueArray[c] = y;
+                    double res = (Math.Sin(i) / i + 1.7) - Math.Cos(i) * 4 * i - 6;
+                    ArrayFunc[Index] = Math.Round(res, 2);
                 }
-                c++;
+                Index++;
             }
-            return valueArray;
+            return ArrayFunc;
         }
     }
 }
