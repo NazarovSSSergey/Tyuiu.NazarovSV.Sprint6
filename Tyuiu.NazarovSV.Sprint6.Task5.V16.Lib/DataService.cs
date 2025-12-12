1687eb70275a8518f7af78a9e107bc9f2112bda8
Сергей Назаров <Tyuiu.NazarovSV@outlook.com>
2025-12-12 20:30:53 +0500

    Task5

---
+++ Tyuiu.NazarovSV.Sprint6.Task5.V16.Lib/DataService.cs
@@ -0,0 +1,25 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint6;
+namespace Tyuiu.NazarovSV.Sprint6.Task5.V16.Lib
+{
+    public class DataService : ISprint6Task5V16
+    {
+        public double[] LoadFromDataFile(string path)
+        {
+            string[] readFromFile = File.ReadAllLines(path);
+            List<double> data = new List<double>();
+            foreach (string line in readFromFile)
+            {
+                if (double.TryParse(line, out double number))
+                {
+                    if (number > 0)
+                    {
+                        data.Add(number);
+
+                    }
+                }
+            }
+            double[] res = data.ToArray();
+            return res;
+        }
+    }
+}
---
+++ Tyuiu.NazarovSV.Sprint6.Task5.V16.Test/DataServiceTest.cs
@@ -0,0 +1,16 @@
+﻿using Tyuiu.NazarovSV.Sprint6.Task5.V16.Lib;
+namespace Tyuiu.NazarovSV.Sprint6.Task5.V16.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            string path = Path.Combine(Path.GetTempPath(), "InPutDataFileTask5V16.txt");
+            DataService dataServcie = new DataService();
+            bool FileStatus = File.Exists(path);
+            Assert.IsTrue(FileStatus);
+        }
+    }
+}
